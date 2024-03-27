9dee52a8030062046757769176e85d9df03dc292
Aaron Djangmah <djangsiness01@gmail.com>
2024-03-27 18:19:24 +0000

    Initial Commit

---
+++ notifications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notifications
+//
+//  Created by Aaron Djangmah on 27/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notifications/notificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  notificationsApp.swift
+//  notifications
+//
+//  Created by Aaron Djangmah on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct notificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
